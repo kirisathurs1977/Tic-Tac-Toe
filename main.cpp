f78c4f3e75c11f1710f595365aca18e16c15300f
kirisathurs1977 <sritharankirisathurs@gmail.com>
2023-09-19 09:25:28 +0530

    begin

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
+char board[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+
 
 int main()
 {
-    cout<<"Hello World";
+
 
     return 0;
 }
