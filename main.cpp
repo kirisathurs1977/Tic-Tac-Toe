4ff4c9f3289c5bfca5d644195f5f3f80481c1334
kirisathurs1977 <sritharankirisathurs@gmail.com>
2023-09-28 21:07:58 +0530

    display

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 using namespace std;
 char board[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-setGamw();
+void display_board(){
+
+    //Rander Game Board LAYOUT
+
+    cout<<"PLAYER - 1 [X]t PLAYER - 2 [O]nn";
+    cout<<"tt     |     |     n";
+    cout<<"tt  "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<" n";
+    cout<<"tt_____|_____|_____n";
+    cout<<"tt     |     |     n";
+    cout<<"tt  "<<board[1][0]<<"  | "<<board[1][1]<<"  |  "<<board[1][2]<<" n";
+    cout<<"tt_____|_____|_____n";
+    cout<<"tt     |     |     n";
+    cout<<"tt  "<<board[2][0]<<"  | "<<board[2][1]<<"  |  "<<board[2][2]<<" n";
+    cout<<"tt     |     |     n";
+}
 int main()
 {
  
