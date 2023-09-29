e1269ba9512fc1fa6ca7d54187bdc1c5454d44b7
kirisathurs1977 <sritharankirisathurs@gmail.com>
2023-09-29 10:31:27 +0530

    main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+#define ROW_COUNT 3;
+int row,column;
+char turn = 'X';
+bool draw = false;
 char board[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+bool gameover(){
+    
+}
 void display_board(){
 
     //Rander Game Board LAYOUT
